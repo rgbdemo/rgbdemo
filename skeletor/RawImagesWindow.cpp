09804add6101096a71c1278cb79d194a17a40da0
Nicolas Burrus <nicolas@burrus.name>
2011-03-14 15:49:09 +0100

    New demo with skeleton.

---
+++ skeletor/RawImagesWindow.cpp
@@ -0,0 +1,158 @@
+/**
+ * This file is part of the rgbdemo project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Nicolas Burrus <nicolas@burrus.name>, (C) 2010, 2011
+ */
+
+#include "RawImagesWindow.h"
+#include "ui_RawImagesWindow.h"
+
+#include "GuiController.h"
+
+#include <ntk/camera/rgbd_frame_recorder.h>
+#include <ntk/camera/rgbd_processor.h>
+#include <ntk/gesture/skeleton.h>
+#include <ntk/utils/opencv_utils.h>
+#ifdef USE_FREENECT
+# include <ntk/camera/kinect_grabber.h>
+#endif
+
+#include <QCloseEvent>
+
+using namespace ntk;
+using namespace cv;
+
+RawImagesWindow::RawImagesWindow(GuiController& controller, QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::RawImagesWindow),
+    m_controller(controller)
+{
+  ui->setupUi(this);
+
+  connect(ui->depthView, SIGNAL(mouseMoved(int,int)),
+          &m_controller, SLOT(on_depth_mouse_moved(int,int)));
+  ui->depthView->setMouseTracking(true);
+
+  ui->action_Show_Object_Detector->setDisabled(true);
+  ui->action_3D_View->setDisabled(true);
+
+  if (m_controller.grabber().isSynchronous())
+    ui->syncMode->setChecked(true);
+}
+
+RawImagesWindow::~RawImagesWindow()
+{
+  delete ui;
+}
+
+void RawImagesWindow :: update(const ntk::RGBDImage& image)
+{
+  if (ui->colorView->isVisible())
+    ui->colorView->setImage(image.rgb());
+
+  if (ui->depthView->isVisible())
+  {
+    double min_dist = m_controller.rgbdProcessor().minDepth();
+    double max_dist = m_controller.rgbdProcessor().maxDepth();
+    cv::Mat1f masked_distance; image.depth().copyTo(masked_distance);
+    apply_mask(masked_distance, image.depthMask());
+    cv::Mat3b depth_as_color;
+    compute_color_encoded_depth(masked_distance, depth_as_color, &min_dist, &max_dist);
+    if (image.skeleton())
+      image.skeleton()->drawOnImage(depth_as_color);
+    ui->depthView->setImage(depth_as_color);
+  }
+  // ui->depthView->setImageAsColor(image.depth(), &min_dist, &max_dist);
+  // ui->depthView->setImage(image.depth(), &min_dist, &max_dist);
+  if (image.userLabels().data && ui->intensityView->isVisible())
+  {
+    const Vec3b colors[] = {
+      Vec3b(255,0,0),
+      Vec3b(255,255,0),
+      Vec3b(255,0,255),
+      Vec3b(255,255,255),
+      Vec3b(0,255,0),
+      Vec3b(0,255,255),
+      Vec3b(0,0,255),
+    };
+    int nb_colors = sizeof(colors) / sizeof(Vec3b);
+
+    cv::Mat3b user_labels(image.userLabels().size());
+    for_all_rc(image.userLabels())
+    {
+      int label = image.userLabels()(r,c);
+      if (label == 0)
+        user_labels(r,c) = Vec3b(0,0,0);
+      else
+        user_labels(r,c) = colors[label%nb_colors];
+    }
+
+    ui->intensityView->setImage(user_labels);
+  }
+
+  int x,y;
+  ui->depthView->getLastMousePos(x,y);
+  m_controller.on_depth_mouse_moved(x,y);
+}
+
+void RawImagesWindow::on_outputDirText_editingFinished()
+{
+  QString dir = ui->outputDirText->text();
+  m_controller.frameRecorder()->setDirectory(dir.toStdString());
+}
+
+void RawImagesWindow::on_action_GrabOneFrame_triggered()
+{
+  m_controller.frameRecorder()->saveCurrentFrame(m_controller.lastImage());
+}
+
+void RawImagesWindow::on_action_Quit_triggered()
+{
+  m_controller.quit();
+}
+
+void RawImagesWindow::on_action_Screen_capture_mode_toggled(bool active)
+{
+  m_controller.setScreenCaptureMode(active);
+}
+
+void RawImagesWindow::on_action_GrabFrames_toggled(bool active)
+{
+  m_controller.setGrabFrames(active);
+}
+
+void RawImagesWindow::on_syncMode_toggled(bool checked)
+{
+  m_controller.grabber().setSynchronous(checked);
+  if (checked)
+    m_controller.grabber().newEvent();
+}
+
+void RawImagesWindow::closeEvent(QCloseEvent *event)
+{
+  ui->action_Quit->trigger();
+  event->accept();
+}
+
+void RawImagesWindow::on_actionPause_toggled(bool active)
+{
+  m_controller.setPaused(active);
+}
+
+void RawImagesWindow::on_actionNext_frame_triggered()
+{
+  m_controller.processOneFrame();
+}
---
+++ skeletor/skeletor.cpp
@@ -0,0 +1,89 @@
+/**
+ * This file is part of the rgbdemo project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Nicolas Burrus <nicolas@burrus.name>, (C) 2010, 2011
+ */
+
+#include <ntk/ntk.h>
+#include <ntk/camera/calibration.h>
+#ifdef USE_OPENNI
+# include <ntk/camera/nite_rgbd_grabber.h>
+#endif
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <sstream>
+#include <iomanip>
+
+#include <ntk/image/sift_gpu.h>
+#include <ntk/camera/opencv_grabber.h>
+#include <ntk/camera/file_grabber.h>
+#include <ntk/camera/rgbd_frame_recorder.h>
+#include <ntk/camera/nite_rgbd_grabber.h>
+#include <ntk/mesh/mesh_generator.h>
+#include "GuiController.h"
+
+#include <QApplication>
+#include <QMetaType>
+
+using namespace ntk;
+using namespace cv;
+
+namespace opt
+{
+  ntk::arg<const char*> dir_prefix("--prefix", "Directory prefix for output", "grab1");
+  ntk::arg<const char*> calibration_file("--calibration", "Calibration file (yml)", 0);
+  ntk::arg<bool> sync("--sync", "Synchronization mode", 0);
+  ntk::arg<bool> high_resolution("--highres", "Low resolution color image.", 0);
+}
+
+int main (int argc, char** argv)
+{
+  arg_base::set_help_option("-h");
+  arg_parse(argc, argv);
+  ntk_debug_level = 1;
+  cv::setBreakOnError(true);
+
+  QApplication::setGraphicsSystem("raster");
+  QApplication app (argc, argv);
+
+  ntk::RGBDProcessor* processor = new NiteProcessor();
+  NiteRGBDGrabber* grabber = new NiteRGBDGrabber();
+  if (opt::high_resolution())
+    grabber->setHighRgbResolution(true);
+    grabber->initialize();
+
+  if (opt::sync())
+    grabber->setSynchronous(true);
+
+  RGBDFrameRecorder frame_recorder (opt::dir_prefix());
+  frame_recorder.setSaveOnlyRaw(false);
+
+  ntk::RGBDCalibration* calib_data = grabber->calibrationData();
+
+  GuiController gui_controller (*grabber, *processor);
+  grabber->addEventListener(&gui_controller);
+  gui_controller.setFrameRecorder(frame_recorder);
+
+  if (opt::sync())
+    gui_controller.setPaused(true);
+
+  grabber->start();
+
+  app.exec();
+}
